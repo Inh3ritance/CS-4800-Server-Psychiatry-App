f84f14898b3071c7473c4bdbcaac53ad29a5d34d
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-02-08 19:32:19 -0800

    Finalization
    Need to implement how to individual query data from Firestore did get the console.log for the full document/collection

--- public/app.js
+++ public/app.js
@@ -1,4 +1,32 @@
-//Snapshot is the a rep of the diff data inside the firestore collection
-db.collection('Inputs').get().then((snapshot) => {
-    console.log(snapshot.docs);
-})
+/*const inputList = document.querySelector('#input-list');
+
+//create element and render Inputs
+function renderInput(doc){
+    //types in index.html
+    let li = document.createElement('li');
+    let hello = document.createElement('span');
+    let greetings = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    hello.textContent = doc.data().Hello;
+    greetings.textContent = doc.data().Greetings;
+
+    li.appendChild(hello);
+    li.appendChild(greetings);
+
+    inputList.appendChild(li);
+}
+*/
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    // something just goes wrong and an error message pops up
+    console.log("Error getting document:", error);
+});
