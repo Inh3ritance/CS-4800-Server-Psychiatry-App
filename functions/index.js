3af561d6b7c08920fdb0b73ad0f729b99c745536
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-02-15 19:14:06 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,6 @@
 const functions = require('firebase-functions');
-//const admin = require('firebase-admin');
-//admin.initializeApp(functions.config().firebase);
-
-
-// // Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 exports.heyWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
