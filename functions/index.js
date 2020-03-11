3103a62828c2616271441d4279abe2be71cbd1e6
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-03-10 21:32:41 -0700

    ffs we got the require() to work.
    Please install express.js when you are done pulling.
    
    'npm install --save express'

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Painful setup process lmao!");
-});
+const express = require('express');
+const app = express();
 
-exports.getData = functions.https.onRequest((req, res) => {
-    admin
-        .firestore()
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
+
+// I know some of these functions seem redundant
+// but for now lets just work with this so 
+// we have an idea what we are doing
+
+app.get('/userQuestionsBot', (req, res) => {
+        db
         .collection('userQuestionsBot')
         .get()
         .then((data) => {
@@ -23,3 +24,123 @@ exports.getData = functions.https.onRequest((req, res) => {
         })
         .catch((err) => console.error(err));
 });
+
+app.get('/userFeelsSad', (req, res) => {
+        db
+        .collection('userFeelsSad')
+        .get()
+        .then((data) => {
+            let myArray = [];
+            data.forEach((doc) => {
+                myArray.push(doc.id);
+            });
+            return res.json(myArray);
+        })
+        .catch((err) => console.error(err));
+});
+
+app.get('/userIsMad', (req, res) => {
+    db
+    .collection('userNullResponse')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.get('/userIsMad', (req, res) => {
+    db
+    .collection('userIsMad')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.get('/userIsDepressed', (req, res) => {
+    db
+    .collection('userIsDepressed')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.get('/userFeelsNervous', (req, res) => {
+    db
+    .collection('userFeelsNervous')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.get('/random', (req, res) => {
+    db
+    .collection('random')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.get('/userQuestionsBot', (req, res) => {
+    db
+    .collection('userQuestionsBot')
+    .get()
+    .then((data) => {
+        let myArray = [];
+        data.forEach((doc) => {
+            myArray.push(doc.id);
+        });
+        return res.json(myArray);
+    })
+    .catch((err) => console.error(err));
+});
+
+//end outputs
+
+
+//Start of inputs
+// app.post(){
+
+// }
+
+
+exports.api = functions.https.onRequest(app);
+
+
+// visit 
+// https://us-central1-cs-4800-backend-server.cloudfunctions.net/api/{functionName}
+// to see the response from the function
+
+
+/*exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Painful setup process lmao!");
+});*/
+
