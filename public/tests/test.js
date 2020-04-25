28056b2c0bce0ad6e8953d1b62eeefcc1487c109
MrChang2 <43765332+MrChang2@users.noreply.github.com>
2020-04-24 18:27:53 -0700

    First QUnit test, removed JQuery folder

--- public/tests/test.js
+++ public/tests/test.js
@@ -1,4 +1,20 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
-  });
+//Functions
+function findHighestScore(list) {
+  var highest = list[0];
+  var highestIndex = 0;
+  for (var x=1;x<list.length;x++) {
+      if (list[x]>highest) {
+          highest = list[x];
+          highestIndex = x;
+      }
+  }
+  var results = [highest, highestIndex];
+  return results;
+}
 
+//Tests
+QUnit.test("HighestScore Works", function( assert) {
+  let list = [1,3,5,2];
+  let result = findHighestScore(list);
+  assert.ok(result[0]==5 && result[1]==2);
+});
